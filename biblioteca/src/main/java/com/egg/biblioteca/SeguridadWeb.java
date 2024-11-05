68f4a20a7a13ba2456af9a276579a2d2be63f309
isaias65 <isaiascallegarisalvador@gmail.com>
2024-11-05 17:38:28 -0500

    Ejercicio 15: agregamos seguridad

---
+++ biblioteca/src/main/java/com/egg/biblioteca/SeguridadWeb.java
@@ -0,0 +1,21 @@
+package com.egg.biblioteca;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SeguridadWeb {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/css/", "/js/", "/img/", "/**").permitAll())
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+}
