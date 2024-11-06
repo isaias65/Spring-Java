036bcfa1b4fbd28e101598c7ca47c5295850c6b5
isaias65 <isaiascallegarisalvador@gmail.com>
2024-11-05 19:41:31 -0500

    Ejercicio 18: agregamos servicio de usuario

--- biblioteca/src/main/java/com/egg/biblioteca/servicios/UsuarioServicio.java
+++ biblioteca/src/main/java/com/egg/biblioteca/servicios/UsuarioServicio.java
@@ -1,8 +1,49 @@
 package com.egg.biblioteca.servicios;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.egg.biblioteca.entidades.Usuario;
+import com.egg.biblioteca.enumeraciones.Rol;
+import com.egg.biblioteca.excepciones.MiException;
+import com.egg.biblioteca.repositorios.UsuarioRepositorio;
+
+
 
 @Service
 public class UsuarioServicio {
-    
+
+    @Autowired
+    private UsuarioRepositorio usuarioRepositorio;
+
+    @Transactional
+    public void registrar(String nombre, String email, String password, String password2) throws Exception{
+        validar(nombre, email, password, password2);
+
+        Usuario usuario = new Usuario();
+
+        usuario.setNombre(nombre);
+        usuario.setEmail(email);
+        usuario.setPassword(password);
+        usuario.setRol(Rol.USER);
+
+        usuarioRepositorio.save(usuario);
+    }
+
+    private void validar(String nombre, String email, String password, String password2) throws MiException {
+
+        if (nombre.isEmpty() || nombre == null) {
+            throw new MiException("el nombre no puede ser nulo o estar vacío");
+        }
+        if (email.isEmpty() || email == null) {
+            throw new MiException("el email no puede ser nulo o estar vacío");
+        }
+        if (password.isEmpty() || password == null || password.length() <= 5) {
+            throw new MiException("La contraseña no puede estar vacía, y debe tener más de 5 dígitos");
+        }
+        if (!password.equals(password2)) {
+            throw new MiException("Las contraseñas ingresadas deben ser iguales");
+        }
+    }
 }
